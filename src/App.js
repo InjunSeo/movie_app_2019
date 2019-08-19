8bd2085eacfb1a68002d962f8f0845177d97c503
injunSeo <fregesaxiom@gmail.com>
2019-08-19 14:33:55 +0900

    des
    (1)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import New_jsx from './new_jsx';
 
+function Food({name, picture}){
+  return <div>
+    <h2>I like {name}!</h2>
+    <img src={picture}  />
+    </div>
+}
+
+const foodILike =[
+  {
+    name: "Tomato",
+    image: "https://images.app.goo.gl/TXV2ijnxALmVVwap8"
+  },
+  {
+    name: "pizza",
+    image: "https://images.app.goo.gl/8fTmJWvXKXLsEAPu7"
+  } 
+];
 function App() {
   return (
     <div className="App">
       <h1>def </h1>
-      <New_jsx />
-      <p>Hello My react page. </p>
+      {foodILike.map(dish =>(
+         <Food name={dish.name} picture={dish.image} /> 
+      ))}
     </div>
   );
 }
--- src/new_jsx.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function New_jsx() {
-    return <h3>The first jsx</h3>
-}
-
-export default New_jsx;
